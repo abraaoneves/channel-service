8ccff024c706b5f70fa915b980d809cd0c4f287a
Abraao Neves <6154363+abraaoneves@users.noreply.github.com>
2022-04-30 13:10:31 -0300

    Change line breaks

--- main.go
+++ main.go
@@ -1,34 +1,30 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
+	"encoding/json"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
-
 type Channel struct {
-    Key         string
-    Composer    string
-    Node        string
+	Key      string
+	Composer string
+	Node     string
 }
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true);
-    router.HandleFunc("/channels", getChannels).Methods("GET");
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/channels", getChannels).Methods("GET")
 
-    http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8000", router)
 }
 
-
 func getChannels(w http.ResponseWriter, r *http.Request) {
-    channels := []Channel {
-        {"key-1234", "compose-1234", "node-1234"},
-        {"key-5678", "compose-5678", "node-5678"},
-    }
-
-
-    w.WriteHeader(http.StatusOK);
-    json.NewEncoder(w).Encode(channels);
+	channels := []Channel{
+		{"key-1234", "compose-1234", "node-1234"},
+		{"key-5678", "compose-5678", "node-5678"},
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(channels)
 }
